d739ca0614b021c2c764890a4e71e62e87196d42
charJKL <11600464+charJKL@users.noreply.github.com>
2022-01-04 10:14:51 +0100

    Remove warning about non-serializable data in reducer.

--- src/store/store.tsx
+++ src/store/store.tsx
@@ -1,11 +1,18 @@
 import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import PagesReducer from "./slice.pages";
 
+
 const Store = configureStore({
+	middleware: (getDefaultMiddleware) => {
+		return getDefaultMiddleware({
+			serializableCheck: {
+				ignoredActions: ['pages/setFiles']
+			}
+		})
+	},
 	reducer: {
 		pages: PagesReducer
 	}
-	
 });
 
 export type StoreState = ReturnType<typeof Store.getState>;
