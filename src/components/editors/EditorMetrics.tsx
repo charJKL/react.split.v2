0141c1b5fd41f8c347af5f8656aeb0496c023b7f
charJKL <11600464+charJKL@users.noreply.github.com>
2022-01-27 19:45:38 +0100

    Fix cursor assigment.

--- src/components/editors/EditorMetrics.tsx
+++ src/components/editors/EditorMetrics.tsx
@@ -5,6 +5,7 @@ import useGetEditorSize from "./useGetEditorSize";
 import useGetPageSize from "./useGetPageSize";
 import useScale from "./useScale";
 import usePosition from "./usePosition";
+import useGetMetrics from "./useGetMetrics";
 import css from "./EditorMetrics.module.scss";
 
 const EditorMetrics = (): JSX.Element =>
@@ -15,6 +16,7 @@ const EditorMetrics = (): JSX.Element =>
 	const pageSize = useGetPageSize(page);
 	const {size, scale, wasScaled, mouseDown: mouseDownScale, mouseUp: mouseUpScale, mouseLeave: mouseLeaveScale, mouseWheel: mouseWheelScale} = useScale(editorSize, pageSize);
 	const {position, isMoving, mouseDown: mouseDownPosition, mouseMove: mouseMovePosition, mouseUp: mouseUpPosition, mouseLeave: mouseLeavePosition, contextmenu : mouseContextPosition} = usePosition(editorRef);
+	useGetMetrics();
 	
 	var toolbars: Array<JSX.Element> = [];
 	var desktop : JSX.Element = <></>;
@@ -37,13 +39,14 @@ const EditorMetrics = (): JSX.Element =>
 	
 	const movingCursor = isMoving ? { cursor: 'move'} : {} ;
 	const scalingCursor = wasScaled ? wasScaled == "scale-out" ? { cursor: 'zoom-out'} : { cursor: 'zoom-in'} : {};
-	const style = { ...size, ...position, ...movingCursor, ...scalingCursor };
+	const styleForEditor = { ...movingCursor, ...scalingCursor };
+	const styleForDesktop = { ...size, ...position, ...movingCursor, ...scalingCursor };
 	return (
-		<div className={css.editor} ref={editorRef} onMouseDown={onMouseDownHandler} onMouseMove={onMouseMoveHandler} onMouseUp={onMouseUpHandler} onMouseLeave={onMouseLeaveHandler} onContextMenu={onMouseContextHandler} onWheel={onMouseWheelHandler}>
+		<div className={css.editor} style={styleForEditor} ref={editorRef} onMouseDown={onMouseDownHandler} onMouseMove={onMouseMoveHandler} onMouseUp={onMouseUpHandler} onMouseLeave={onMouseLeaveHandler} onContextMenu={onMouseContextHandler} onWheel={onMouseWheelHandler}>
 			<div className={css.toolbars}>
 				<label>🔍 {scale.x.toFixed(2)} / 1.00</label>
 			</div>
-			<div className={css.desktop} style={style}>
+			<div className={css.desktop} style={styleForDesktop}>
 				{desktop}
 			</div>
 		</div>
