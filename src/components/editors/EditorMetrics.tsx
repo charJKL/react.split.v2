2005b2a4e80e1c6497eeb2052c411351f01d60b4
charJKL <11600464+charJKL@users.noreply.github.com>
2022-01-27 19:15:35 +0100

    Refactor `EditorMetrics` event handlers.

--- src/components/editors/EditorMetrics.tsx
+++ src/components/editors/EditorMetrics.tsx
@@ -1,5 +1,5 @@
-import React, { useRef } from "react";
-import { selectSelectedPage } from "../../store/slice.pages";
+import React, { useRef, MouseEvent } from "react";
+import { loadPage, selectSelectedPage } from "../../store/slice.pages";
 import { useAppSelector } from "../../store/store.hooks";
 import useGetEditorSize from "./useGetEditorSize";
 import useGetPageSize from "./useGetPageSize";
@@ -13,8 +13,8 @@ const EditorMetrics = (): JSX.Element =>
 	const editorRef = useRef<HTMLDivElement>(null);
 	const editorSize = useGetEditorSize(editorRef);
 	const pageSize = useGetPageSize(page);
-	const [size, scale] = useScale(editorSize, pageSize);
-	const {position, mousedown, mousemove, mouseup, mouseleave, contextmenu} = usePosition(editorRef);
+	const {size, scale, mouseDown: mouseDownScale, mouseUp: mouseUpScale, mouseLeave: mouseLeaveScale, mouseWheel: mouseWheelScale} = useScale(editorSize, pageSize);
+	const {position, mousedown: mouseDownPosition, mousemove: mouseMovePosition, mouseup: mouseUpPosition, mouseleave: mouseLeavePosition, contextmenu : mouseContextPosition} = usePosition(editorRef);
 	
 	var toolbars: Array<JSX.Element> = [];
 	var desktop : JSX.Element = <></>;
@@ -28,9 +28,16 @@ const EditorMetrics = (): JSX.Element =>
 		)
 	}
 	
+	const onMouseDownHandler = (e: MouseEvent) => { mouseDownScale(e); mouseDownPosition(e); }
+	const onMouseMoveHandler = (e: MouseEvent) => { mouseMovePosition(e); }
+	const onMouseUpHandler = (e: MouseEvent) => { mouseUpScale(e); mouseDownPosition(e); }
+	const onMouseLeaveHandler = (e: MouseEvent) => { mouseLeaveScale(e); mouseLeavePosition(e); }
+	const onMouseContextHandler = (e: MouseEvent) => { mouseContextPosition(e); }
+	const onMouseWheelHandler = (e: MouseEvent) => { mouseWheelScale(e); }
+	
 	const style = { ...size, ...position };
 	return (
-		<div className={css.editor} ref={editorRef} onMouseDown={mousedown} onMouseMove={mousemove} onMouseUp={mouseup} onMouseLeave={mouseleave} onContextMenu={contextmenu}>
+		<div className={css.editor} ref={editorRef} onMouseDown={onMouseDownHandler} onMouseMove={onMouseMoveHandler} onMouseUp={onMouseUpHandler} onMouseLeave={onMouseLeaveHandler} onContextMenu={onMouseContextHandler} onWheel={onMouseWheelHandler}>
 			<div className={css.toolbars}>
 				<label>🔍 {scale.x.toFixed(2)} / 1.00</label>
 			</div>
