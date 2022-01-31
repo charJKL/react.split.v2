de05b9f8e51a2bfbc24b8a9e5825f8aaf9e95dee
charJKL <11600464+charJKL@users.noreply.github.com>
2022-01-31 21:25:10 +0100

    Refactor `useAllowREpositioning` to use `useRef` hook.

--- src/components/editors/hooks/useAllowRepositioning.tsx
+++ src/components/editors/hooks/useAllowRepositioning.tsx
@@ -1,12 +1,12 @@
-import { RefObject, useEffect, useState } from "react";
+import { RefObject, useEffect, useRef } from "react";
 import type { Position } from "../types/Position";
 import { MouseButton } from "../types/MouseButton";
 import useDisplacement from "./useDisplacement";
 
 const useAllowRepositioning = (editor: RefObject<HTMLElement>, desktop: RefObject<HTMLElement>) =>
 {
-	const [position, setPosition] = useState<Position>({left: 0, top: 0});
-	const [initalPosition, setInitalPosition] = useState<Position>({left: 0, top: 0});
+	const position = useRef<Position>({left: 0, top: 0});
+	const initalPosition = useRef<Position>({left: 0, top: 0});
 	const {displacementing, displacement} = useDisplacement(MouseButton.right);
 	
 	useEffect(() =>{
@@ -19,23 +19,16 @@ const useAllowRepositioning = (editor: RefObject<HTMLElement>, desktop: RefObjec
 			{
 				const editorPosition = editor.current!.getBoundingClientRect();
 				const desktopPosition = desktop.current!.getBoundingClientRect();
-				const position = 
-				{
-					left: desktopPosition.left - editorPosition.left,
-					top: desktopPosition.top - editorPosition.top,
-				}
-				setInitalPosition(position);
+				initalPosition.current.left = desktopPosition.left - editorPosition.left;
+				initalPosition.current.top = desktopPosition.top - editorPosition.top;
 			}
 		}
 		const mouseMove = (e: MouseEvent) =>
 		{
-			if(displacementing === true)
+			if(displacementing.current === true)
 			{
-				const position = {
-					left: initalPosition.left + displacement.left,
-					top: initalPosition.top + displacement.top,
-				}
-				setPosition(position);
+				position.current.left = initalPosition.current.left + displacement.current.left;
+				position.current.top = initalPosition.current.top + displacement.current.top;
 			}
 		}
 		const contextmenu = (e: MouseEvent) =>
@@ -52,61 +45,9 @@ const useAllowRepositioning = (editor: RefObject<HTMLElement>, desktop: RefObjec
 			element.removeEventListener('mousemove', mouseMove);
 			element.removeEventListener('contextmenu', contextmenu);
 		}
-	}, [editor, desktop, initalPosition, displacement, displacementing]);
+	}, [editor, desktop]);
 	
-	return {position, positioning: displacementing};
+	return {position: position.current, positioning: displacementing.current};
 }
 
 export default useAllowRepositioning;
-
-
-/*
-import type { Position } from "../types/Position";
-import { MouseButton } from "../types/MouseButton";
-import useDisplacement from "./useDisplacement";
-import { declareInterface } from "@babel/types";
-
-const usePosition = (element: RefObject<HTMLElement>, desktop: RefObject<HTMLDivElement> ) => 
-{
-	// const [position, setPosition] = useState<Position>({left: 0, top: 0});
-	const [initalPosition, setInitalPosition] = useState<Position>({left: 0, top: 0});
-	const {displacementing, displacement} = useDisplacement(MouseButton.right);
-	
-	const mouseDown = (e: MouseEvent) =>
-	{
-		if(element.current === null) return;
-		if(desktop.current === null) return;
-		if(e.button === MouseButton.right)
-		{
-			const editorPosition = element.current.getBoundingClientRect();
-			const desktopPosition = desktop.current.getBoundingClientRect();
-			setInitalPosition({left: desktopPosition.left - editorPosition.left, top: desktopPosition.top - editorPosition.top });
-		}
-	}
-	
-	const position = {
-		left: initalPosition.left + displacement.left,
-		top: initalPosition.top + displacement.top,
-	}
-	
-	useEffect(() =>{
-		if(element.current == null) return;
-		
-		const contextmenu = (e: MouseEvent) =>
-		{
-			e.preventDefault();
-		}
-		
-		console.log('adding listeners');
-		element.current.addEventListener('mousedown', mouseDown);
-		element.current.addEventListener('contextmenu', contextmenu);
-		return () => {
-			if(element.current == null) return;
-			element.current.removeEventListener('mousedown', mouseDown);
-			element.current.removeEventListener('contextmenu', contextmenu);
-		}
-	}, [element.current, element.current]);
-	
-	return {position, positioning: displacementing};
-}
-*/
