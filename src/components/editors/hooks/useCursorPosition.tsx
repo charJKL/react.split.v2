965a93c8e1d5956c35e63a11ae4e689b6ea41ac4
charJKL <11600464+charJKL@users.noreply.github.com>
2022-02-01 16:52:58 +0100

    Change logic in `useCursorPosition` hook.

--- src/components/editors/hooks/useCursorPosition.tsx
+++ src/components/editors/hooks/useCursorPosition.tsx
@@ -1,18 +1,25 @@
-import { MouseEvent, useState } from "react";
+import { useEffect, useState } from "react";
 import type {Position} from "../types/Position";
 
 const useCursorPosition = (editorPosition: Position, desktopPosition: Position) =>
 {
-	const [cursor, setCursor] = useState<Position>({left: 0, top: 0});
+	const [cursor, setCursor] = useState<Position>({left:0, top: 0});
 	
-	const mouseMove = (e: MouseEvent) =>
-	{
-		const left = e.clientX - desktopPosition.left - editorPosition.left;
-		const top = e.clientY - desktopPosition.top - editorPosition.top;
-		setCursor({left, top});
-	}
+	useEffect(() =>{
+		const mouseMove = (e: MouseEvent) =>
+		{
+			const left = e.clientX - desktopPosition.left - editorPosition.left;
+			const top = e.clientY - desktopPosition.top - editorPosition.top;
+			setCursor({left, top});
+		}
+		
+		document.addEventListener('mousemove', mouseMove);
+		return () => {
+			document.removeEventListener('mousemove', mouseMove);
+		}
+	}, [editorPosition, desktopPosition])
 	
-	return {cursor, mouseMove};
+	return {cursor};
 }
 
 export default useCursorPosition;
