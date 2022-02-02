d85f6eb20477bafce262d3f8d5a64ab252e9c41d
charJKL <11600464+charJKL@users.noreply.github.com>
2022-02-02 18:38:49 +0100

    Implement `Main` component.

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,12 +1,23 @@
+import { useRef } from "react";
 import {CustomElement, CustomElementProps} from "../type/CustomElement";
-
+import useGetBoundingRect from "./hooks/useGetBoundingRect";
 import EditorMetrics from './editors/EditorMetrics';
+import EditorText from './editors/EditorText';
+import css from "./Main.module.scss";
 
 const Main : CustomElement = ({className} : CustomElementProps ) : JSX.Element =>
 {
+	const mainRef = useRef<HTMLDivElement>(null);
+	const rect = useGetBoundingRect(mainRef);
+
+	const sizeEditorMetrics = {width: rect.width / 2};
+	const editorMetricsSize = {width: rect.width / 2};
+	const classForMain = [className, css.main].join(" ");
 	return (
-		<main className={className}>
-			<EditorMetrics />
+		<main className={classForMain} ref={mainRef}>
+			<EditorMetrics style={ sizeEditorMetrics } />
+			<div className={css.separator}></div>
+			<EditorText style={ editorMetricsSize } />
 		</main>
 	)
 } 
