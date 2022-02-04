863bb6c787c5d8f2cb9f410282f9bb7647001c8b
charJKL <11600464+charJKL@users.noreply.github.com>
2022-02-04 12:49:26 +0100

    Add status indicator for `EditorTextStatus`.

--- src/components/editors/EditorText.tsx
+++ src/components/editors/EditorText.tsx
@@ -3,13 +3,15 @@ import {CustomElement, CustomElementProps} from "../../type/CustomElement";
 import { useAppSelector, useAppDispatch } from "../../store/store.hooks";
 import { isPageLoaded, selectSelectedPage } from "../../store/slice.pages";
 import { selectMetricsForPage } from "../../store/slice.metrics";
-import { readPage } from "../../store/slice.ocrs";
+import { readPage, selectOcrForPage } from "../../store/slice.ocrs";
+import EditorTextStatus from "./EditorTextStatus";
 import css from "./EditorText.module.scss";
 
 const EditorText : CustomElement = ({className, style} : CustomElementProps) : JSX.Element =>
 {
 	const page = useAppSelector(selectSelectedPage);
 	const metrics = useAppSelector(selectMetricsForPage(page));
+	const ocr = useAppSelector(selectOcrForPage(page));
 	const dispatch = useAppDispatch();
 	
 	const onProcessHandler = (e: MouseEvent) =>
@@ -23,10 +25,29 @@ const EditorText : CustomElement = ({className, style} : CustomElementProps) : J
 		}
 	}
 	
+	var status : JSX.Element = <></>;
+	var desktop : JSX.Element = <></>;
+	
+	if(ocr)
+	{
+		status = <EditorTextStatus className={css.status} status={ocr.status} details={ocr.details}/>;
+	}
+	if(page && ocr && page.status === "Loaded")
+	{
+		desktop = (
+			<>
+				{ocr.lines.map((line, i) => <div key={i}>{line.text}</div>)}
+			</>
+		)
+	}
+	
 	const classForEditor = [css.editor, className].join(" ");
 	return (<div className={classForEditor} style={style}>
 		<button onClick={onProcessHandler} style={{margin: '50px'}}>Proccess file</button>
-		<canvas id="testing"></canvas>
+		{ status }
+		<div>
+			{desktop}
+		</div>
 	</div>)
 }
 
---
+++ src/components/editors/EditorTextStatus.tsx
@@ -0,0 +1,41 @@
+import { CustomElementProps } from "../../type/CustomElement";
+import { Ocr } from "../../store/slice.ocrs";
+import css from "./EditorTextStatus.module.scss";
+
+type EditorTextStatusPropsExt = {status: Ocr['status'], details: Ocr['details']}
+type EditorTextStatusProps = CustomElementProps & EditorTextStatusPropsExt;
+
+const EditorTextStatus = ({className, status, details, ...props}: EditorTextStatusProps) : JSX.Element =>
+{
+	const classForStatus = [className, css.block].join(" ");
+	switch(status)
+	{
+		case "Idle":
+			return <></>;
+		
+		case "Preprocessing":
+		case "Initializaing":
+		case "Error":
+			return (
+			<div className={classForStatus}>
+				<div className={css.status}>{status}</div>
+				<div className={css.details}>{details}</div>
+			</div>)
+			
+		case "Parsing":
+			const percent = (details as number * 100);
+			return (
+			<div className={classForStatus}>
+				<div className={css.status}>{status}</div>
+				<div className={css.details}>{percent.toFixed(0)} / 100 %</div>
+				<div className={css.progressBar}><div style={{width: `${percent}%`}}></div></div>
+			</div>)
+		
+		case "Parsed":
+			return <></>;
+		
+		
+	}
+}
+
+export default EditorTextStatus;
