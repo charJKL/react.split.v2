8581c1acad1d5c917615261270bf3ca26e9b5b13
charJKL <11600464+charJKL@users.noreply.github.com>
2022-02-06 18:01:32 +0100

    Fix bug with invalid type for `MouseButton` events.

--- src/components/types/MouseButton.tsx
+++ src/components/types/MouseButton.tsx
@@ -1,8 +1,12 @@
+import {MouseEvent as MouseEventReact} from "react";
+
 export enum MouseButton { Left = 0, Right = 2 };
 export enum MouseButtonPress { Left = 1, Right = 2, Whell = 4 }
 
-export const isLeftButtonClicked = (event: MouseEvent) : boolean => event.button === MouseButton.Left;
-export const isRightButtonClicked = (event: MouseEvent) : boolean => event.button === MouseButton.Right;
+type MouseEventMerge = MouseEvent | MouseEventReact;
 
-export const isLeftButtonPressed = (event: MouseEvent) : boolean => event.buttons === MouseButtonPress.Left;
-export const isRightButtonPressed = (event: MouseEvent) : boolean => event.buttons === MouseButtonPress.Right;
+export const isLeftButtonClicked = (event: MouseEventMerge) : boolean => event.button === MouseButton.Left;
+export const isRightButtonClicked = (event: MouseEventMerge) : boolean => event.button === MouseButton.Right;
+
+export const isLeftButtonPressed = (event: MouseEventMerge) : boolean => event.buttons === MouseButtonPress.Left;
+export const isRightButtonPressed = (event: MouseEventMerge) : boolean => event.buttons === MouseButtonPress.Right;
