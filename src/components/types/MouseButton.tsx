d6a46050366633370cab93a0da735a2063d207e6
charJKL <11600464+charJKL@users.noreply.github.com>
2022-02-06 18:11:47 +0100

    Add new `isNoneButtonPressed` state for `MouseButton`.

--- src/components/types/MouseButton.tsx
+++ src/components/types/MouseButton.tsx
@@ -1,12 +1,13 @@
 import {MouseEvent as MouseEventReact} from "react";
 
 export enum MouseButton { Left = 0, Right = 2 };
-export enum MouseButtonPress { Left = 1, Right = 2, Whell = 4 }
+export enum MouseButtonPress { None = 0, Left = 1, Right = 2, Whell = 4 }
 
 type MouseEventMerge = MouseEvent | MouseEventReact;
 
 export const isLeftButtonClicked = (event: MouseEventMerge) : boolean => event.button === MouseButton.Left;
 export const isRightButtonClicked = (event: MouseEventMerge) : boolean => event.button === MouseButton.Right;
 
+export const isNoneButtonPressed = (event: MouseEventMerge) : boolean => event.buttons === MouseButtonPress.None;
 export const isLeftButtonPressed = (event: MouseEventMerge) : boolean => event.buttons === MouseButtonPress.Left;
 export const isRightButtonPressed = (event: MouseEventMerge) : boolean => event.buttons === MouseButtonPress.Right;
