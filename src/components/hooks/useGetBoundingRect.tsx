63568bf09c560ad12bc906ecd67083860df64314
charJKL <11600464+charJKL@users.noreply.github.com>
2022-02-06 16:17:04 +0100

    Refactor `useGetBoundingRect` hook.

--- src/components/hooks/useGetBoundingRect.tsx
+++ src/components/hooks/useGetBoundingRect.tsx
@@ -1,13 +1,23 @@
-import { RefObject, useEffect, useState } from "react";
+import { RefObject, useEffect, useLayoutEffect, useState } from "react";
 
-const useGetBoundingRect = (element: RefObject<HTMLDivElement>) : DOMRect =>
+const useGetBoundingRect = (element: HTMLDivElement | null) : DOMRect =>
 {
 	const [boundingRect, setBoundingRect] = useState<DOMRect>(new DOMRect(0,0,0,0));
 	
-	useEffect(() => {
-		if(element.current) setBoundingRect(element.current.getBoundingClientRect());
+	useLayoutEffect(() => {
+		if(element) setBoundingRect(element.getBoundingClientRect());
 	}, [element]);
 	
+	useEffect(() =>{
+		if(element === null)  return;
+		const observer = new ResizeObserver((entries) => { setBoundingRect(entries[0].contentRect) });
+		observer.observe(element);
+		return () =>
+		{
+			observer.unobserve(element);
+		}
+	}, [element]);
+
 	return boundingRect;
 }
 
