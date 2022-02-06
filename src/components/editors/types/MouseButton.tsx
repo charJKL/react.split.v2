9d520938cf87c308d68c652ab664134577fdc7a4
charJKL <11600464+charJKL@users.noreply.github.com>
2022-02-06 13:56:06 +0100

    Implement mouse button helper functions.

--- src/components/editors/types/MouseButton.tsx
+++ src/components/editors/types/MouseButton.tsx
@@ -1,1 +1,4 @@
 export enum MouseButton { Left = 0, Right = 2 };
+
+export const isLeftButtonClicked = (button: number) : boolean => button === MouseButton.Left;
+export const isRightButtonClicked = (button: number) : boolean => button === MouseButton.Right;
