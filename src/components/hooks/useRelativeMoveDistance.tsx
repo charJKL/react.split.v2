5aca8fdefe6f32da5bf68ce41219609340120145
charJKL <11600464+charJKL@users.noreply.github.com>
2022-02-06 16:18:53 +0100

    Implement `useRelativeMoveDistance` hook.

---
+++ src/components/hooks/useRelativeMoveDistance.tsx
@@ -0,0 +1,32 @@
+import { useEffect, useRef } from "react";
+
+type Position = {x: 0, y: 0};
+
+const useGetMouseMoveDistance = () : number =>
+{
+	const relative = useRef<Position>({x: 0, y: 0});
+	
+	useEffect(() => {
+		const mousedown = (e: MouseEvent) => 
+		{
+			relative.current.x = 0;
+			relative.current.y = 0;
+		};
+		const mousemove = (e: MouseEvent) => 
+		{
+			relative.current.x += e.movementX;
+			relative.current.y += e.movementY;
+		};
+		
+		document.addEventListener('mousedown', mousedown);
+		document.addEventListener('mousemove', mousemove);
+		return () => {
+			document.removeEventListener('mousedown', mousedown);
+			document.removeEventListener('mousemove', mousemove);
+		}
+	}, []);
+	
+	return Math.sqrt(Math.abs(relative.current.x) + Math.abs(relative.current.y));
+}
+
+export default useGetMouseMoveDistance;
