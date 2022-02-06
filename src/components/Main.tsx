24f7b23edeba069c6deb2dcbf8e224fa40f03501
charJKL <11600464+charJKL@users.noreply.github.com>
2022-02-06 16:18:35 +0100

    Update `Main` component to follow changes in hooks implementation.

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -4,17 +4,18 @@ import useGetBoundingRect from "./hooks/useGetBoundingRect";
 import EditorMetrics from './editors/EditorMetrics';
 import EditorText from './editors/EditorText';
 import css from "./Main.module.scss";
+import useRefElement from "./hooks/useRefElement";
 
 const Main = ({className} : CustomHTMLAttributes ) : JSX.Element =>
 {
-	const mainRef = useRef<HTMLDivElement>(null);
+	const [mainRef, setMainRef] = useRefElement<HTMLDivElement>(null);
 	const rect = useGetBoundingRect(mainRef);
 
 	const sizeEditorMetrics = {width: rect.width / 2};
 	const editorMetricsSize = {width: rect.width / 2};
 	const classForMain = [className, css.main].join(" ");
 	return (
-		<main className={classForMain} ref={mainRef}>
+		<main className={classForMain} ref={setMainRef}>
 			<EditorMetrics style={ sizeEditorMetrics } />
 			<div className={css.separator}></div>
 			<EditorText style={ editorMetricsSize } />
