f4bc82f4664a906e2ad984daf5570933d5915591
charJKL <11600464+charJKL@users.noreply.github.com>
2022-02-06 16:34:35 +0100

    Refactor `MouseButton` logic. There is diffrence between `button` vs `buttons`.

--- src/components/types/MouseButton.tsx
+++ src/components/types/MouseButton.tsx
@@ -1,4 +1,8 @@
 export enum MouseButton { Left = 0, Right = 2 };
+export enum MouseButtonPress { Left = 1, Right = 2, Whell = 4 }
 
-export const isLeftButtonClicked = (button: number) : boolean => button === MouseButton.Left;
-export const isRightButtonClicked = (button: number) : boolean => button === MouseButton.Right;
+export const isLeftButtonClicked = (event: MouseEvent) : boolean => event.button === MouseButton.Left;
+export const isRightButtonClicked = (event: MouseEvent) : boolean => event.button === MouseButton.Right;
+
+export const isLeftButtonPressed = (event: MouseEvent) : boolean => event.buttons === MouseButtonPress.Left;
+export const isRightButtonPressed = (event: MouseEvent) : boolean => event.buttons === MouseButtonPress.Right;
