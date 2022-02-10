6c357408f92c068ccbc73de47bb4c6455edf1166
charJKL <11600464+charJKL@users.noreply.github.com>
2022-02-10 15:22:20 +0100

    Refactor `EditorMetrics` to follow changes with `null`.

--- src/components/editors/EditorMetrics.tsx
+++ src/components/editors/EditorMetrics.tsx
@@ -1,55 +1,56 @@
-import {MouseEvent, useState, WheelEvent, ChangeEvent } from "react";
-import { CustomHTMLAttributes } from "../types/CustomHTMLAttributes";
-import { useAppSelector, useAppDispatch } from "../../store/store.hooks";
-import { Page, selectSelectedPage } from "../../store/slice.pages";
-import { Metric, MetricLineNames, selectMetricsForPage, updateMetricValue, updateStatus } from "../../store/slice.metrics";
-import { Size } from "./types/Size";
-import { isLeftButtonClicked, isNoneButtonPressed } from "../types/MouseButton";
-import { calculateScale, applayScaleToSize, applayScaleToMetrics} from "./Editor";
-import useGetBoundingRect from "../hooks/useGetBoundingRect";
+import { useState, MouseEvent, WheelEvent } from "react";
+import { isLeftButtonClicked, isNoneButtonPressed } from "../../types";
+import { useAppDispatch, useAppSelector } from "../../store/store.hooks";
+import { MetricLineNames, selectMetricsForPage, updateMetricValue } from "../../store/slice.metrics";
+import { selectSelectedPage } from "../../store/slice.pages";
+import { applayScaleToMetrics, applayScaleToSize } from "./Editor";
+import useRefElement from "../hooks/useRefElement";
 import useGetPageSize from "./hooks/useGetPageSize";
-import useCursorPosition from "./hooks/useCursorPosition";
-import useResolveObjectBeingHovered from "./hooks/useResolveObjectBeingHovered";
+import useInitializeSetting from "./hooks/useInitializeSetting";
 import useGetDesktopPosition from "./hooks/useGetDesktopPosition";
 import useGetScale from "./hooks/useGetScale";
+import useCursorPosition from "./hooks/useCursorPosition";
+import useResolveObjectBeingHovered from "./hooks/useResolveObjectBeingHovered";
 import LayerPage from "./LayerPage";
-import LayerLines from "./LayerLines";
 import LayerHighlight from "./LayerHighlight";
+import LayerLines from "./LayerLines";
 import css from "./EditorMetrics.module.scss";
-import useRefElement from "./../hooks/useRefElement";
+
+interface EditorMetricsProps
+{
+	style?: {width: number};
+}
 
 type SelectableObject = MetricLineNames | null;
-type LayerProps = {className: string, page: Page, metric: Metric, desktopSize: Size};
-const EditorMetrics = ({className, style} : CustomHTMLAttributes): JSX.Element =>
+
+const EditorMetrics = ({style}: EditorMetricsProps) : JSX.Element =>
 {
 	const page = useAppSelector(selectSelectedPage);
-	const metric = useAppSelector(selectMetricsForPage(page));
+	const metric = useAppSelector(selectMetricsForPage(page ? page.id : ""));
 	const dispatch = useAppDispatch();
 	
+	const editorName = "EditorMetrics";
+	const pageId = page ? page.id : "";
 	const [editorRef, setEditorRef] = useRefElement<HTMLDivElement>(null);
 	const [desktopRef, setDesktopRef] = useRefElement<HTMLDivElement>(null);
-	const editorSize = useGetBoundingRect(editorRef);
 
-	const desktopInitalPosition = {left: 0, top: 0};
-	const [desktopPosition, isPositioning] = useGetDesktopPosition(editorRef, desktopInitalPosition);
-	
-	const pageSize = useGetPageSize(page);
-	const initalScale = calculateScale(editorSize, pageSize);
-	const [scale, isScaling] = useGetScale(editorRef, initalScale);
-	const scaledDesktopSize = applayScaleToSize(pageSize, scale);
-	const scaledMetrics = applayScaleToMetrics(metric, scale);
-	
-	const cursorPosition = useCursorPosition(editorRef, desktopRef);
+	useInitializeSetting(editorRef, editorName, pageId);
+	const [desktopPosition, isPositioning] = useGetDesktopPosition(editorRef, editorName, pageId);
+	const [scale, isScaling] = useGetScale(editorRef, editorName, pageId);
+	const pageSize = useGetPageSize(pageId);
+	const scaledDesktopSize = pageSize && scale && applayScaleToSize(pageSize, scale);
+	const scaledMetrics = metric && scale && applayScaleToMetrics(metric, scale);
+
 	const [objectSelected, selectObject] = useState<SelectableObject>(null);
-	const objectHovered = useResolveObjectBeingHovered(scaledMetrics, objectSelected, cursorPosition);
-
+	const cursorPosition = useCursorPosition(editorRef, desktopRef);
+	const objectHovered = useResolveObjectBeingHovered(scaledMetrics, cursorPosition, objectSelected);
 	const onMouseDown = (e: MouseEvent) =>
 	{
 		if(objectHovered && isLeftButtonClicked(e)) selectObject(objectHovered);
 	}
 	const onMouseMove = (e: MouseEvent) =>
 	{
-		if(page && metric && objectHovered && objectSelected)
+		if(page && metric && scale && objectHovered && objectSelected)
 		{
 			switch(objectSelected)
 			{
@@ -90,28 +91,18 @@ const EditorMetrics = ({className, style} : CustomHTMLAttributes): JSX.Element =
 	
 	var toolbars: Array<JSX.Element> = [];
 	var layers: Array<JSX.Element> = [];
-	
-	if(page && scaledMetrics && page.status === "Loaded")
+	if(page && scaledDesktopSize && scaledMetrics)
 	{
 		layers.push(<LayerPage key="editor-metric-page" className={css.page} page={page} metric={scaledMetrics} desktopSize={scaledDesktopSize} />);
-		toolbars.push(<>🔍 {scale.x.toFixed(2)} / {scale.y.toFixed(2)}</>);
-		toolbars.push(<>➡ {cursorPosition.left.toFixed(2)} / {cursorPosition.top.toFixed(2)}</>);
-	}
-	if(page && metric && scaledMetrics)
-	{
-		layers.push(<LayerLines key="editor-metric-lines" className={css.metricLines} page={page} metric={scaledMetrics} desktopSize={scaledDesktopSize} objectHovered={objectHovered}/>)
-	}
-	if(page && metric && scaledMetrics)
-	{
 		layers.push(<LayerHighlight key="editor-metric-highlight" className={css.highlight} page={page} metric={scaledMetrics} desktopSize={scaledDesktopSize}/>);
+		layers.push(<LayerLines key="editor-metric-lines" className={css.metricLines} page={page} metric={scaledMetrics} desktopSize={scaledDesktopSize} objectHovered={objectHovered}/>)
+		toolbars.push(<>🔍 {scale.x.toFixed(2)} / {scale.y.toFixed(2)}</>);
 	}
-	
 	const cursor = { cursor: resolveCursor(isScaling, isPositioning, objectHovered, objectSelected) }
 	const styleForEditor = { ...style, ...cursor };
 	const styleForDesktop = { ...scaledDesktopSize, ...desktopPosition };
-	const classNameForEditor = [className, css.editor].join(" ");
 	return (
-		<div className={classNameForEditor} style={styleForEditor} ref={setEditorRef} onMouseDown={onMouseDown} onMouseMove={onMouseMove} onMouseUp={onMouseUp} onWheel={onWheel}>
+		<div className={css.editor} style={styleForEditor} ref={setEditorRef} onMouseDown={onMouseDown} onMouseMove={onMouseMove} onMouseUp={onMouseUp} onWheel={onWheel}>
 			<div className={css.toolbars}>
 				{ toolbars.map((toolbar, i) => <label key={i}>{ toolbar }</label> ) }
 			</div>
@@ -131,5 +122,5 @@ const resolveCursor = (isScaling: isScalingType, isPositioning: boolean, isObjec
 	if(isPositioning) return 'move';
 }
 
-export type { SelectableObject, LayerProps };
+export type { SelectableObject };
 export default EditorMetrics;
