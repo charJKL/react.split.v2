5af3cc3fd78813962fed7996dc599ed5e555a363
charJKL <11600464+charJKL@users.noreply.github.com>
2022-02-10 15:42:17 +0100

    Center image at starup.

--- src/components/editors/hooks/useInitializeSetting.tsx
+++ src/components/editors/hooks/useInitializeSetting.tsx
@@ -1,6 +1,6 @@
 import { initializeSetting, isSettingInitialized } from "../../../store/slice.gui";
 import { useAppDispatch, useAppSelector } from "../../../store/store.hooks";
-import { calculateScale } from "../Editor";
+import { applayScaleToSize, calculateScale } from "../Editor";
 import useGetBoundingRect from "../../hooks/useGetBoundingRect";
 import useGetPageSize from "./useGetPageSize";
 
@@ -13,11 +13,12 @@ const useInitializeSetting = (editor: HTMLDivElement | null, editorName: string,
 	
 	if(isInitialized === false && editorSize && pageSize)
 	{
-		const setting =
-		{
-			position: {left: -1, top: -1},
-			scale: calculateScale(editorSize, pageSize)
-		}
+		const padding = {x: 20, y: 20};
+		const editorSizeWithPadding = {width: editorSize.width - padding.x, height: editorSize.height - padding.y};
+		const scale = calculateScale(editorSizeWithPadding, pageSize);
+		const size = applayScaleToSize(pageSize, scale);
+		const position = { left: (editorSize.width - size.width) / 2, top: (editorSize.height - size.height) / 2 };
+		const setting = {scale, position};
 		dispatch(initializeSetting({editorName, pageId, setting}));
 	}
 }
