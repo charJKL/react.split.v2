3921f1c8cb7977f91b7a1c648a0ffa7289e4fccb
charJKL <11600464+charJKL@users.noreply.github.com>
2022-02-13 18:41:02 +0100

    Follow changes of `loadFile`. Fix bug with z-index positioning.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,7 @@
 import React from "react";
 import {CustomHTMLAttributes} from "../types";
 import { useAppDispatch, useAppSelector } from "../store/store.hooks";
-import { loadFile } from "../store/store.file";
-import { selectProjects } from "../store/slice.projects";
+import { selectProjects, loadFile } from "../store/slice.projects";
 import LoadFileInput from "./common/LoadFileInput";
 import Select from "./header/Select";
 import ProjectOption from "./header/ProjectOption";
@@ -17,14 +16,7 @@ const Header = ({className} : CustomHTMLAttributes) : JSX.Element =>
 	{
 		dispatch(loadFile(files));
 	}
-	
-	/*
-	const onProjectChange = (e: ChangeEvent) =>
-	{
-		console.log('sss');
-	}
-	*/
-	
+
 	const cssHeader = [className, css.header].join(' ');
 	return (
 		<header className={cssHeader}>
