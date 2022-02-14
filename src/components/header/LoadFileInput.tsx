29d863b0995352f9ebfa421d6e20b23843ed9a0f
charJKL <11600464+charJKL@users.noreply.github.com>
2022-02-14 19:20:05 +0100

    Refactor style for `LoadFileInput`. Disable when no project is selected.

--- src/components/header/LoadFileInput.tsx
+++ src/components/header/LoadFileInput.tsx
@@ -1,8 +1,8 @@
 import React, { useRef, ChangeEvent } from "react";
 import { useAppDispatch, useAppSelector } from "../../store/store.hooks";
-import { loadFile } from "../../store/slice.projects";
-import { selectTooltip, updateTooltip } from "../../store/slice.gui";
-import Tooltip from "../common/Tooltip";
+import { loadFile, selectSelectedProject } from "../../store/slice.projects";
+import { updateTooltip } from "../../store/slice.gui";
+
 import css from "./LoadFileInput.module.scss";
 
 interface LoadFileInput 
@@ -13,7 +13,7 @@ interface LoadFileInput
 
 const LoadFileInput = ({className, children}: LoadFileInput) : JSX.Element =>
 {
-	const showStartHereTooltip = useAppSelector(selectTooltip("startHere"));
+	const project = useAppSelector(selectSelectedProject);
 	const dispatch = useAppDispatch();
 	const fileInputRef = useRef<HTMLInputElement>(null);
 	
@@ -25,18 +25,15 @@ const LoadFileInput = ({className, children}: LoadFileInput) : JSX.Element =>
 	{
 		const files = e.target.files ?? [];
 		const array = Array.from(files);
-		dispatch(updateTooltip({tooltip: "startHere", value: false})); // it's importat to dispose `startHere` tooltip before `loadFile` action.
 		dispatch(loadFile(array));
 	}
 	
+	const isDisabled = project ? false : true;
 	const classNameForWrapper = [className, css.wrapper].join(" ");
 	return (
 		<div className={classNameForWrapper}>
-			<button className={css.button} onClick={onClickRedirectEvent}>{children}</button>
+			<button className={css.button} disabled={isDisabled} onClick={onClickRedirectEvent}>{children}</button>
 			<input className={css.files} type="file" accept="image/*" multiple ref={fileInputRef} onChange={onChangeLoadSelectedFile} />
-			<Tooltip show={showStartHereTooltip} alignment="top-left" width="300%">
-				Start here from loading image from which you want to extract text.
-			</Tooltip>
 		</div>
 	)
 }
