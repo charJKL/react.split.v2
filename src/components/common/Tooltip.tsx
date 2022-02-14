fcb88d71c972c2b247641eaec186c58cb1b66580
charJKL <11600464+charJKL@users.noreply.github.com>
2022-02-14 12:26:34 +0100

    Change `Tooltip` show/hide logic. Use class instead of style display poperty.

--- src/components/common/Tooltip.tsx
+++ src/components/common/Tooltip.tsx
@@ -13,12 +13,11 @@ interface TooltipProps
 const Tooltip = ({className, show, alignment, width, children}: TooltipProps) : JSX.Element =>
 {
 	const widthStyle = width ? width : '100%';
-	const displayStyle = show ? 'block' : 'none';
-	const styleForTooltip = {display: displayStyle, width: widthStyle };
-	const classNameForTooltip = [className, css.tooltip, css[alignment]].join(" ");
+	const styleForTooltip = {width: widthStyle };
+	const classShowTooltip = show ? css.show : "";
+	const classNameForTooltip = [className, css.tooltip, css[alignment], classShowTooltip].join(" ");
 	return (
 		<div className={classNameForTooltip} style={styleForTooltip}>
-			<div className={css.arrow}></div>
 			{ children }
 		</div>
 	)
