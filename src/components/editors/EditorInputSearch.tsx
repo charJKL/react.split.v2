5acac9e427189f3b9f197ce5280a2bb4731c1934
charJKL <11600464+charJKL@users.noreply.github.com>
2022-02-14 12:27:27 +0100

    Add information tooltip for `EditorInputSearch`.

--- src/components/editors/EditorInputSearch.tsx
+++ src/components/editors/EditorInputSearch.tsx
@@ -1,7 +1,8 @@
-import { ChangeEvent, FocusEvent, KeyboardEvent } from "react";
+import { ChangeEvent, FocusEvent, KeyboardEvent, useState } from "react";
 import { selectPage, selectSearching, setSearching } from "../../store/slice.gui";
 import { Page, selectPageByName } from "../../store/slice.pages";
 import { useAppDispatch, useAppSelector } from "../../store/store.hooks";
+import Tooltip from "../common/Tooltip";
 import css from "./EditorInputSearch.module.scss";
 
 interface EditorInputSearchProps
@@ -14,6 +15,7 @@ const EditorInputSearch = ({className, value} : EditorInputSearchProps) : JSX.El
 {
 	const searching = useAppSelector(selectSearching);
 	const results = useAppSelector(selectPageByName(searching ?? ""));
+	const [showTooltip, setShowTooltip] = useState<boolean>(false);
 	const resultsCount = searching && results && results.length;
 	const result = resultsCount == 1 ? results[0] ?? null : null;
 	const dispatch = useAppDispatch();
@@ -23,8 +25,13 @@ const EditorInputSearch = ({className, value} : EditorInputSearchProps) : JSX.El
 		const value = e.target.value;
 		dispatch(setSearching(value));
 	}
+	const onFocus = (e: FocusEvent) =>
+	{
+		setShowTooltip(true);
+	}
 	const onBlurName = (e: FocusEvent) =>
 	{
+		setShowTooltip(false)
 		dispatch(setSearching(null));
 	}
 	const onKeyDown  = (e: KeyboardEvent<HTMLInputElement>) => 
@@ -52,8 +59,12 @@ const EditorInputSearch = ({className, value} : EditorInputSearchProps) : JSX.El
 	const classNameForResult = [css.result, foundOneIndicator].join(" ");
 	return (
 		<div className={css.wrapper}>
-			<input className={classNameForInput} name="name" type="text" value={value} onChange={onChangeName} onBlur={onBlurName} onKeyDown={onKeyDown}/>
+			<input className={classNameForInput} name="name" type="text" autoComplete="off" value={value} onChange={onChangeName} onFocus={onFocus} onBlur={onBlurName} onKeyDown={onKeyDown}/>
 			<div className={classNameForResult}>{resultsCount}</div>
+			<Tooltip show={showTooltip} alignment="bottom-left">
+				<p>Provided search patter must match only one element.</p>
+				<p>Press <span>Enter</span> to select page, <span>Esc</span> to cancel.</p>
+			</Tooltip>
 		</div>
 	)
 }
