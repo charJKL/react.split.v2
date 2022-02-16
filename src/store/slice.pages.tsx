ffc79c52b28fd17d1c999546b459ef09cc2b65fe
charJKL <11600464+charJKL@users.noreply.github.com>
2022-02-16 17:09:24 +0100

    Refactor `slice.page`. Add `source` property. Add new status `Stalled`.

--- src/store/slice.pages.tsx
+++ src/store/slice.pages.tsx
@@ -3,17 +3,21 @@ import { GetStoreState, StoreDispatch, StoreState } from "./store";
 import {resetState} from "./store.reset";
 import StoreException from "./lib/storeException";
 
-type PageStatus = "Idle" | "Restored" | "Loading" | "Loaded" | "Error";
+type PageStatus = "Idle" | "Restored" | "Stalled" | "Loading" | "Loaded" | "Error";
 type Key = string;
+type Source = {lastModified: number, name: string, size: number, type: string }
 type StatusValue = {id: Key; status: PageStatus; }
 type SizeValue = {id: Key; width: number, height: number; }
+type UrlValue = {id: Key; url: string; }
+
 
 type Page = 
 {
 	id: Key,
 	status: PageStatus,
-	url: string,
 	name: string,
+	source: Source,
+	url?: string,
 	width?: number,
 	height?: number,
 }
@@ -57,12 +61,19 @@ const Pages = createSlice({
 			if(page === undefined) throw new StoreException(`You updating status of nonexistent page`, action.payload);
 			page.width = action.payload.width;
 			page.height = action.payload.height;
+		},
+		updateUrl: (state, action: PayloadAction<UrlValue>) =>
+		{
+			const id = action.payload.id;
+			const page = state.entities[id];
+			if(page === undefined) throw new StoreException(`You updating url of nonexistent page`, action.payload);
+			page.url = action.payload.url;
 		}
 	},
 	extraReducers: (builder) => { builder
 		.addCase(LoadPagesAction, (state, action) => {
 			const pages = action.payload;
-			Object.values(pages.entities).forEach((page) => page.status = "Restored"); // pages load from localStorage aren't loaded.
+			Object.values(pages.entities).forEach((page) => {page.status = "Restored"; page.url = undefined}); // pages load from localStorage aren't loaded.
 			return pages;
 		})
 		.addCase(resetState, (state, action) => {
@@ -71,7 +82,7 @@ const Pages = createSlice({
 	}
 });
 
-type PageLoaded = Page & {status: "Loaded"; width: number; height: number;}
+type PageLoaded = Page & {status: "Loaded"; url: string, width: number; height: number;}
 const isPageIdle = (page: Page) : boolean => page.status === "Idle";
 const isPageLoaded = (page: Page) : page is PageLoaded => page.status === "Loaded";
 
@@ -81,6 +92,12 @@ const loadPage = (pageId: Key) => (dispatch: StoreDispatch, getState: GetStoreSt
 	const { updateStatus, setSize } = Pages.actions;
 	const page = pages.entities[pageId];
 	if(page === undefined) throw new StoreException(`You try load page with doesn't exist.`, {type: 'pages/loadPage', payload: pageId})
+
+	if(page.status === "Restored")
+	{
+		dispatch(updateStatus({id: pageId, status: "Stalled"}));
+		return;
+	}
 	
 	const image = new Image();
 	dispatch(updateStatus({id: pageId, status: "Loading"}));
@@ -94,7 +111,19 @@ const loadPage = (pageId: Key) => (dispatch: StoreDispatch, getState: GetStoreSt
 	image.addEventListener('error', (e: Event) => {
 		dispatch(updateStatus({id: pageId, status: "Error"}));
 	});
-	image.src = page.url;
+	image.src = page.url as string; // we are sure that url will be set.
+}
+
+const updateUrl = (urlValue: UrlValue) => (dispatch: StoreDispatch, getState: GetStoreState) =>
+{
+	const { pages } = getState();
+	const { updateUrl } = Pages.actions;
+	const page = pages.entities[urlValue.id];
+	if(page === undefined) throw new StoreException(`You try update url for page with doesn't exist.`, {type: 'pages/updateUrl', payload: urlValue})
+	
+	console.log("ble", page);
+	dispatch(updateUrl(urlValue));
+	if(page.status === "Stalled") dispatch(loadPage(urlValue.id));
 }
 
 export const selectPageIds = (state: StoreState) => state.pages.ids;
@@ -103,7 +132,7 @@ export const selectPageByName = (name: string) => (state: StoreState) : Array<Pa
 export const selectSelectedPage = (state: StoreState) : Page | null => state.gui.selected ? state.pages.entities[state.gui.selected] ?? null : null;
 
 export const { addPage } = Pages.actions;
-export { isPageIdle, isPageLoaded, loadPage };
+export { isPageIdle, isPageLoaded, loadPage, updateUrl };
 
-export type { Page, PageLoaded };
+export type { Page, Source, PageLoaded };
 export default Pages;
